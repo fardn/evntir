6354ef3e595c2a947f64e2a17e857c43b7d5f87c
fardin <far.alis@gmail.com>
2020-03-21 03:57:19 +0430

    [base_page.html] login and logout actions added to menu

--- eventinfo/urls.py
+++ eventinfo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('events/', views.event_list, name='event_list'),
     path('event/<int:event_id>/', views.event_detail, name='event_detail'),
     path('event/<int:event_id>/booking/', views.event_booking, name='event_booking'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- eventinfo/views.py
+++ eventinfo/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 from eventinfo.models import Event, Event_types
 from eventinfo.forms import EventSearchForm
@@ -42,3 +45,35 @@ def event_booking(request, event_id):
     }
 
     return render(request, 'eventinfo/event_booking.html', context)
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('eventinfo:event_list'))
+        else:
+
+            # TODO: LOAD ERROR MESSAGES IN POPUP FORM
+
+            context = {
+                'user': username,
+                'error': 'کاربری یا این مشخصات یافت نشد.',
+            }
+            return render(request, 'eventinfo/events.html/', context)
+
+    else:
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('eventinfo:event_list'))
+        else:
+            context = {}
+
+    return render(request, 'eventinfo/events.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('eventinfo:event_list'))
