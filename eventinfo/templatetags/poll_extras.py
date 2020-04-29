c35d3949e0a94005a8e0782f6a9d170714cdeb6e
fardin <far.alis@gmail.com>
2020-04-30 00:52:26 +0430

    Booking | Ticket seats display in form if order exist

--- eventinfo/templatetags/poll_extras.py
+++ eventinfo/templatetags/poll_extras.py
@@ -9,3 +9,12 @@ def is_bookmarked(value, arg):
         return True
     else:
         return False
+
+
+@register.filter(name='item_seats')
+def item_seats(value, arg):
+    if value[0].items.filter(ticket_id=arg, ordered=False).exists():
+        item = value[0].items.filter(ticket_id=arg, ordered=False)
+        return item[0].seats
+    else:
+        return 0
